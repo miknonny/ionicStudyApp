49a1b43c5ccabae05f2d72c0e0953942bd29f2cb
nonnycode <miknonny@gmail.com>
2014-08-10 20:51:24 +0100

    popup

--- www/js/app.js
+++ www/js/app.js
@@ -5,22 +5,67 @@
 // the 2nd parameter is an array of 'requires'
 var myApp = angular.module('starter', ['ionic']);
 
-myApp.controller('PopCtrl', ['$scope', '$ionicPopover',
-  function($scope, $ionicPopover){
-    $ionicPopover.fromTemplateUrl('my-popover.html', {
-      scope: $scope
-    })
-    .then(function (popover) {
-      $scope.popover = popover;
+myApp.controller('PopCtrl', ['$scope', '$ionicPopup', '$timeout',
+ function ($scope, $ionicPopup, $timeout) {
+    
+    //Triggered on button click or on someother button.
+    $scope.showPopup = function () {
+      $scope.data = {};
+      
+      //An elaborate custom popup.
+      var myPopup = $ionicPopup.show({
+        template: '<input type="password" ng-model="data.wifi">',
+        subtitle: 'please use normal things',
+        scope: $scope,
+        buttons: [
+          {text: 'Cancel'},
+          {
+            text: '<b>Save</b>',
+            type: 'button-positive',
+            onTap: function (e) {
+              if (!$scope.data.wifi) {
+
+                //Dont allow the user to close unless he enters the
+                //wifi password.
+                e.preventDefault();
+              } else {
+                return $scope.wifi.data;
+              }
+            }
+          }
+        ]
+      });
+      
+      myPopup.then(function (res) {
+        console.log('Tapped', res);
+      });
+
+      $timeout(function () {
+        myPopup.close();
+      }, 3000);
+    };
+
+    $scope.showConfirm = function () {
+      var confirmPopup = $ionicPopup.confirm({
+        title: 'Consume Ice cream',
+        template: 'Are you sure you want to eat this icecream?'
+      })
+      .then(function (res) {
+        if (res) {
+          console.log('Are you sure');
+        } else {
+          console.log('You are not sure');
+        }
+      });
+    };
+
+    //An alert Dialogue.
+    $scope.showAlert(function () {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Do not eat that',
+        template: 'it might taste good'
+      });
     });
-
-    $scope.openPopover = function ($event) {
-      $scope.popover.show($event);
-    };
-
-    $scope.closePopover = function () {
-      $scope.popover.hide();
-    };
   }
 ]);
 
